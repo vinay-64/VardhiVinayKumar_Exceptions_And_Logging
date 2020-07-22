399a981252ac4befd1996970ded15cf20ecf1965
vinaykumar1237 <56100155+vinaykumar1237@users.noreply.github.com>
2020-07-22 09:22:23 +0530

    Delete Calculate.java

--- InterestCalculation/src/main/java/com/vinay/InterestCalculation/Calculate.java
+++
@@ -1,8 +0,0 @@
-package com.vinay.InterestCalculation;
-
-public interface Calculate {
-	public abstract double calculateSimpleInterest(double principleAmount, double time, double rateOfInterest);
-	
-	public abstract double calculateCompoundInterest(double principleAmount, double time, double rateOfInterest);
-
-}
